1c5b2073e52ab2cd06d8cb825f57ff689ed8a7b3
Bernd Vogt <bernd.vogt@sourcepit.org>
2012-04-22 22:17:48 +0200

    Added FileUtils

--- src/main/java/org/sourcepit/common/utils/file/FileUtils.java
+++ src/main/java/org/sourcepit/common/utils/file/FileUtils.java
@@ -7,6 +7,12 @@
 package org.sourcepit.common.utils.file;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
 
 /**
  * @author Bernd Vogt <bernd.vogt@sourcepit.org>
@@ -16,6 +22,26 @@ public final class FileUtils
    private FileUtils()
    {
       super();
+   }
+
+   public static FileInputStream openInputStream(File file, boolean createOnDemand) throws IOException
+   {
+      if (createOnDemand && !file.exists())
+      {
+         org.apache.commons.io.FileUtils.forceMkdir(file.getParentFile());
+         file.createNewFile();
+      }
+      return org.apache.commons.io.FileUtils.openInputStream(file);
+   }
+
+   public static FileOutputStream openOutputStream(File file, boolean createOnDemand) throws IOException
+   {
+      if (createOnDemand && !file.exists())
+      {
+         org.apache.commons.io.FileUtils.forceMkdir(file.getParentFile());
+         file.createNewFile();
+      }
+      return org.apache.commons.io.FileUtils.openOutputStream(file);
    }
 
    public static void accept(File file, FileVisitor visitor)
