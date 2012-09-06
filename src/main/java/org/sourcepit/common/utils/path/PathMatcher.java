6aa6e4dafe6cbb2a166152014ea7b508d73be5a3
Bernd Vogt <bernd.vogt@sourcepit.org>
2012-09-06 23:01:21 +0200

    Escape regex in separator string

--- src/main/java/org/sourcepit/common/utils/path/PathMatcher.java
+++ src/main/java/org/sourcepit/common/utils/path/PathMatcher.java
@@ -67,7 +67,7 @@ public class PathMatcher
          }
          else
          {
-            return patternString.split(patternSeparator);
+            return patternString.split(escRegEx(patternSeparator));
          }
       }
 
