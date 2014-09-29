159c66c64e132d3a4c251c5bbf0550a075df085a
Bernd Vogt <bernd.vogt@sourcepit.org>
2014-09-29 10:16:47 +0200

    #1: Apache License Version 2.0

--- src/main/java/org/sourcepit/common/utils/lang/ThrowablePipe.java
+++ src/main/java/org/sourcepit/common/utils/lang/ThrowablePipe.java
@@ -1,7 +1,17 @@
-/**
- * Copyright (c) 2011 Sourcepit.org contributors and others. All rights reserved. This program and the accompanying
- * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+/*
+ * Copyright 2014 Bernd Vogt and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.sourcepit.common.utils.lang;
@@ -17,19 +27,19 @@ import java.util.List;
 public interface ThrowablePipe extends Iterable<Throwable>, Serializable
 {
    List<Throwable> getThrowables();
-   
+
    boolean isEmpty();
-   
+
    Throwable getCause();
-   
+
    List<Throwable> getFollowers();
-   
+
    void add(Throwable throwable);
-   
+
    <T extends Throwable> T adapt(Class<T> type);
 
    <T extends Throwable> void adaptAndThrow(Class<T> type) throws T;
-   
+
    Throwable toPipedThrowable();
 
    void throwPipe();
@@ -37,7 +47,7 @@ public interface ThrowablePipe extends Iterable<Throwable>, Serializable
    StackTraceElement[] getStackTrace();
 
    void printStackTrace();
-   
+
    void printStackTrace(final PrintWriter printWriter);
 
    void printStackTrace(final PrintStream printStream);
